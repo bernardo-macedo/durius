3d4d0d4094f8bc13ec54a45ceeac05acacbb4d1d
Bernardo <bernardolbmacedo@gmail.com>
2024-01-31 00:40:37 +0000

    New version

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
+// init Swiper:
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  slidesPerView: 3,
+  spaceBetween: 30,
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // Breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+// responsive menu toggle
 const btn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
 
