4e0ec09f13c2d681dad9ff5b900e02a799516f93
bernardo-macedo <131559083+bernardo-macedo@users.noreply.github.com>
2024-02-01 01:10:48 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,9 @@ module.exports = {
       colors: {
         primaryRed: "#601919",
         primaryLightRed: "#803b3b",
-        brightRed: "hsl(12, 88%, 59%)",
-        brightRedLight: "hsl(12, 88%, 69%)",
-        brightRedSupLight: "hsl(12, 88%, 95%)",
         darkBlue: "hsl(228, 39%, 23%)",
         darkGrayishBlue: "hsl(227, 12%, 61%)",
         veryDarkBlue: "hsl(233, 12%, 13%)",
-        veryPaleRed: "hsl(13, 100%, 96%)",
         veryLightGray: "hsl(0, 0%, 98%)",
       },
       backgroundImage: {
